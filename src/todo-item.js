98e305a5d8102bc0535cdbdeabc4288e6e766071
hpham474 <hpham474@gmail.com>
2024-06-10 01:59:09 -0500

    add a project value to todo items and made the variables private

--- src/todo-item.js
+++ src/todo-item.js
@@ -1,51 +1,61 @@
 class TodoItem {
-    title;
-    description;
-    dueDate;
-    priority;
-    notes;
+    #title;
+    #project;
+    #description;
+    #dueDate;
+    #priority;
+    #notes;
 
-    constructor(title, description = "", dueDate, priority = 0, notes = "") {
-        this.title = title;
-        this.description = description;
-        this.dueDate = dueDate;
-        this.priority = priority;
-        this.notes = notes;
+    constructor(title, project, dueDate, description, priority, notes) {
+        this.#title = title;
+        this.#project = project;
+        this.#description = description;
+        this.#dueDate = dueDate;
+        this.#priority = priority;
+        this.#notes = notes;
     }
 
     get title() {
-        return this.title;
+        return this.#title;
+    }
+
+    get project() {
+        return this.#project;
     }
 
     get description() {
-        return this.description;
+        return this.#description;
     }
 
     get dueDate() {
-        return this.dueDate;
+        return this.#dueDate;
     }
 
     get priority() {
-        return this.priority;
+        return this.#priority;
     }
 
     get notes() {
-        return this.notes;
+        return this.#notes;
+    }
+
+    set project(value) {
+        this.#project = value;
     }
 
     set description(value) {
-        this.description = value;
+        this.#description = value;
     }
 
     set dueDate(value) {
-        this.dueDate = value;
+        this.#dueDate = value;
     }
 
     set priority(value) {
-        this.priority = value;
+        this.#priority = value;
     }
 
     set notes(value) {
-        this.notes = value;
+        this.#notes = value;
     }
 }
