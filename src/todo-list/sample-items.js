369d1c529985ed1a13cfa735b3175d3eeb5b9fe0
hpham474 <hpham474@gmail.com>
2024-06-17 01:09:29 -0500

    change dates for sample tasks

--- src/todo-list/sample-items.js
+++ src/todo-list/sample-items.js
@@ -1,38 +1,44 @@
 import TodoItem from './todo-item';
+import { format, add, sub } from "date-fns";
 
-const date = new Date();
+const today = new Date();
+const tommorow = add(today, {days: 1});
+const yesterday = sub(today, {days: 1});
+const threeDaysFromNow = add(today, {days: 3});
+const weekFromNow = add(today, {days: 7});
+
 const todoItem1 = new TodoItem(
     "title1", 
     "project1", 
-    date,
+    format(today, "yyyy-MM-dd"),
     "description1",
     1,
     "notes1");
 const todoItem2 = new TodoItem(
     "title2", 
     "project1", 
-    date,
+    format(tommorow, "yyyy-MM-dd"),
     "description2",
     2,
     "notes2");
 const todoItem3 = new TodoItem(
     "title3", 
     "project2", 
-    date,
+    format(yesterday, "yyyy-MM-dd"),
     "description3",
     1,
     "notes3");
 const todoItem4 = new TodoItem(
     "title4", 
     "project2", 
-    date,
+    format(threeDaysFromNow, "yyyy-MM-dd"),
     "description4",
     1,
     "notes4");
 const todoItem5 = new TodoItem(
     "title5", 
     "project3", 
-    date,
+    format(weekFromNow, "yyyy-MM-dd"),
     "description5",
     2,
     "notes5");
