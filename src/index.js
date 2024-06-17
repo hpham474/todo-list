196925394ecd768064b19d8f35430b8d39f5d52c
hpham474 <hpham474@gmail.com>
2024-06-16 20:39:52 -0500

    add query for non project buttons in sidebar

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
 import './style.css';
 import getTodoList from './todo-list/default-list';
 import generateProjectSidebar from './interface/sidebar';
-import generateInbox from './pages/inbox'
+import generateInbox from './pages/inbox';
 
 const todoList = getTodoList();
 const projects = todoList.projects;
+
+const addTask = document.querySelector(".addTask");
+const inbox = document.querySelector(".inbox");
+const today = document.querySelector(".today");
+const urgent = document.querySelector(".urgent");
 
 generateProjectSidebar(projects);
 
