c82b5d0f7a1a7a6e4144a31b48c04eb5ece83239
hpham474 <hpham474@gmail.com>
2024-06-19 03:41:18 -0500

    change webpack.config.js to production mode

--- webpack.config.js
+++ webpack.config.js
@@ -3,13 +3,13 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     entry: './src/index.js',
-    mode: 'development',
+    mode: 'production',
     output: {
         filename: 'main.js',
         path: path.resolve(__dirname, 'dist'),
         clean: true,
     },
-    devtool: 'inline-source-map',
+    devtool: 'source-map',
     module: {
         rules: [
             {
